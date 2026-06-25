1b3982b333bb8df70804415c1113872bc8b57ee1
wign <tigfiragnafatur1933@gmail.com>
2026-06-25 17:37:07 +0700

    fix(guilds): import StickersModule in GuildsModule to resolve dependency injection error

--- src/guilds/guilds.module.ts
+++ src/guilds/guilds.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DiscordModule } from '../discord/discord.module';
+import { StickersModule } from '../stickers/stickers.module';
 import { GuildsController } from './guilds.controller';
 import { GuildsService } from './guilds.service';
 
 @Module({
-  imports: [DiscordModule],
+  imports: [DiscordModule, StickersModule],
   controllers: [GuildsController],
   providers: [GuildsService],
   exports: [GuildsService],
